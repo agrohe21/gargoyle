f95b905ceebdb54aa07e88b1c177e3dade26e292
agrohe21 <agrohe21@gmail.com>
2020-02-27 10:02:45 -0600

    changes related to switch to pg and user agrohe21wq

--- env/ws_files/bwmon_extract.sh
+++ env/ws_files/bwmon_extract.sh
@@ -1,7 +1,10 @@
 cd /home/agrohe21/projects/gargoyle/env/ws_files
 
-./kitchen.sh -norep -file=/home/agrohe21/projects/gargoyle/content/public/gargoyle/run_bwmon_gather.kjb 
+current_date_time=`date +%Y-%m-%d-%H-%M-%S`
+echo $current_date_time
+
+./kitchen.sh -norep -file=/home/agrohe21/projects/gargoyle/content/public/gargoyle/run_bwmon_gather.kjb -logfile=/home/agrohe21/projects/log/gargoyle/bwmon_gather-$current_date_time
 
 EXIT_CODE=$?
-echo Error:$EXIT_CODE
+echo ErrorCode:$EXIT_CODE
 exit $EXIT_CODE
