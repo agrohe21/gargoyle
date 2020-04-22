0fcdd3c00932643414ee06a5cf90a82e36f4f55d
agrohe21 <agrohe21@gmail.com>
2020-04-22 15:24:03 -0500

    missed some reporting files

--- config/s_conn.sql
+++ config/s_conn.sql
@@ -1,4 +1,4 @@
-﻿SELECT sum(total_bytes)/1000000 as total_mbytes, t3.host_name, t2.wan_domain, t2.fqdn, t4.day_month, count(*)
+SELECT sum(total_bytes)/1000000 as total_mbytes, t3.host_name, t2.wan_domain, t2.fqdn, t4.day_month, count(*)
   FROM public.f_connection t1
   inner join public.dim_wan_host t2 on t2.wan_host_id = t1.wan_host_id
   inner join public.dim_hosts t3 on t3.host_id = t1.host_id
@@ -41,14 +41,15 @@ group by t2.wan_domain
 order by sum(total_bytes)/1000000 desc
 limit 20
 
-SELECT t4.hour_day, t3.host_name, t2.wan_domain, round(sum(total_bytes)/1000000::numeric,2) as total_mbytes, count(*)
+SELECT t4.hour_day, t3.host_name, t2.wan_domain, fqdn, round(sum(total_bytes)/1000000::numeric,2) as total_mbytes, count(*)
   FROM public.f_connection t1
   inner join public.dim_wan_host t2 on t2.wan_host_id = t1.wan_host_id
   inner join public.dim_hosts t3 on t3.host_id = t1.host_id
   inner join public.dim_date  t4 on t4.date_key = t1.date_key
 Where date_trunc('day', t4."DATE") = CURRENT_DATE
   and t3.host_owner = 'eva'
-group by t4.hour_day, host_name, wan_domain
+  and host_name = 'eva-table-FireHD10'
+group by host_name, t4.hour_day, wan_domain, fqdn
 having sum(total_bytes)/1000000 > 1
 order by hour_day asc, sum(total_bytes) desc
 
