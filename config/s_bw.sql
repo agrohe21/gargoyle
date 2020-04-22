668a8a6610e17fd1ccddef3a0c2a201688a7748d
agrohe21 <agrohe21@gmail.com>
2020-04-22 15:20:42 -0500

    updated reporting files

---
+++ config/s_bw.sql
@@ -0,0 +1,4 @@
+﻿select count(*), sum(total_bytes)/1000000, sum(download_bytes)/1000000, sum(upload_bytes)/1000000, t2.host_owner, sum(total_bytes)-sum(download_bytes)-sum(upload_bytes)
+from f_bandwidth t1
+inner join dim_hosts t2 on t2.host_id = t1.host_id
+group by t2.host_owner
